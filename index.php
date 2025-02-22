f48832e00913b8a3012906b05cd07727c2fd9252
Orkblan <bossolay@hotmail.fr>
2025-02-22 19:10:38 +0100

    feat: add architecture and first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./assets/css/header-footer.css">
+  <link rel="stylesheet" href="./assets/css/style.css">
+  <title>Family-Collection</title>
+</head>
+
+<body>
+
+  <?php include("./components/footer.php") ?>
