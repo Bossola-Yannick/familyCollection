f780e62ccda2bb3738b69104b4d07f377ff2a436
Orkblan <bossolay@hotmail.fr>
2025-02-22 20:13:06 +0100

    feat: add Bdd connexion and page connexion

--- components/header.php
+++ components/header.php
@@ -29,7 +29,7 @@ if (!empty($_SESSION)) {
       <!-- pas connecté  -->
       <div class="logo-box">
         <a href="../index.php">
-          <img src="../assets/img/accueil-logo.png" class="logo-header" alt="connexion" />
+          <img src="#" class="logo-header" alt="accueil" />
         </a>
       </div>
 
@@ -37,47 +37,28 @@ if (!empty($_SESSION)) {
 
       <div class="logo-login">
         <a href="./connexion.php">
-          <img src="../assets/img/utilisateur.png">
+          <img src="#" alt="connexion">
         </a>
       </div>
-    <?php elseif ($_SESSION['userRole'] == "admin") : ?>
-
-      <!-- connecté ADMIN-->
-      <div class="logo-box">
-        <a href="../index.php">
-          <img src="../assets/img/accueil-logo.png" class="logo-header" alt="connexion" />
-        </a>
-      </div>
-      <img class="quiz-logo" src="../assets/img/titre-logo.png" />
-      <form method="post" action="" class="box-login-disconnect">
-        <a href="./admin.php" class="icon-account header-user-logo" type="submit" name="user-profil">
-          <div class="box-account">
-            <img src="../assets/img/redSuit.png" class="logo-admin" />
-          </div>
-        </a>
-        <button class="icon-account" type="submit" name="logout">
-          <img src="../assets/img/deconnexion.png" alt="deconnexion" />
-        </button>
-      </form>
     <?php else : ?>
 
       <!-- connecté USER -->
       <div class="logo-box">
         <a href="../index.php">
-          <img src="../assets/img/accueil-logo.png" class="logo-header" alt="connexion" />
+          <img src="#" class="logo-header" alt="accueil" />
         </a>
       </div>
       <img class="quiz-logo" src="../assets/img/titre-logo.png" />
       <form method="post" action="" class="box-login-disconnect">
         <a href="./profil.php" class="icon-account header-user-logo" type="submit" name="user-profil">
           <div class="box-account">
-            <img src="../assets/img/utilisateur.png" />
+            <img src="#" alt="avatar" />
           </div>
           <!-- penser a refair une session=>userNumber -->
           <p class="login "><?= "{ " . $_SESSION['userNumber'] . " }" ?></p>
         </a>
         <button class="icon-account" type="submit" name="logout">
-          <img src="../assets/img/deconnexion.png" alt="deconnexion" />
+          <img src="#" alt="deconnexion" />
         </button>
       </form>
     <?php endif; ?>
---
+++ pages/newItem.php
@@ -0,0 +1,3 @@
+<?php include('../components/header.php') ?>
+<main></main>
+<?php include('../components/footer.php') ?>
